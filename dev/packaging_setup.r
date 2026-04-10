9b637aeb4517611f3be8661aa0e92177fa48669d
Luveen Wadhwani <luveenw@gmail.com>
2026-04-09 20:20:24 -0400

    chore(package-test): setup tweaks

--- dev/packaging_setup.r
+++ dev/packaging_setup.r
@@ -1,6 +1,23 @@
-# Install dependencies - this is already taken care of by the DESCRIPTION file, but you can run this to make sure everything is installed
-install.packages(c("fansi", "downlit", "pkgdown", "devtools", "roxygen2", "usethis"))
-# pak::pak(c("fansi", "downlit", "pkgdown", "devtools", "roxygen2", "usethis"))
+# Install dependencies - this is already taken care of by the DESCRIPTION file,
+# but you can run this to make sure everything is installed.
+install_if_missing <- function(packages) {
+    for (p in packages) {
+        if (!requireNamespace(p, quietly = TRUE)) {
+            install.packages(
+                p,
+                dependencies = TRUE, repos = "https://cloud.r-project.org/"
+            )
+        }
+        library(p, character.only = TRUE)
+    }
+}
+
+install_if_missing(c(
+    "fansi", "downlit", "pkgdown", "devtools", "roxygen2", "usethis"
+))
+
+# pak::pak(c(
+#   "fansi", "downlit", "pkgdown", "devtools", "roxygen2", "usethis"))
 
 # One-time calls to add Rcpp and Armadillo as dependencies for C++ compilation
 usethis::use_package("Rcpp", type = "LinkingTo")
@@ -9,7 +26,7 @@ usethis::use_package("RcppArmadillo", type = "LinkingTo")
 # one-time call to check documentation
 # devtools::check(args = c("--as-cran"))
 
-# install and build from GitHub (WIP)
+# (WIP) install and build from GitHub
 # remotes::install_github(
 #   "luveenw/occPlus@luveenw_test_plan_3", build_vignettes = FALSE)
 
@@ -17,9 +34,10 @@ usethis::use_package("RcppArmadillo", type = "LinkingTo")
 # out the line above this:
 devtools::install(build_vignettes = FALSE)
 
-# Build binary package
+# (WIP) Build binary package
 #
-# NOTE: THIS GENERATES A PLATFORM-SPECIFIC PACKAGE. PACKAGE FILES GENERATED ON WINDOWS WON'T RUN ON MACOS, FOR EXAMPLE
+# NOTE: THIS GENERATES A PLATFORM-SPECIFIC PACKAGE. PACKAGE FILES
+# GENERATED ON WINDOWS WON'T RUN ON MACOS, FOR EXAMPLE
 #
 # devtools::build(binary = TRUE)
 
