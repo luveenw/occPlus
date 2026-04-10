ccbf2dbae4d94acdd728607f5ff258831bb759e8
Luveen Wadhwani <luveenw@gmail.com>
2026-04-09 20:06:32 -0400

    chore(package-test): add wip packaging setup script, mcmc support code; update autogen doc, ignorefiles

---
+++ dev/packaging_setup.r
@@ -0,0 +1,27 @@
+# Install dependencies - this is already taken care of by the DESCRIPTION file, but you can run this to make sure everything is installed
+# install.packages(c("fansi", "downlit", "pkgdown", "devtools", "roxygen2", "usethis"))
+# pak::pak(c("fansi", "downlit", "pkgdown", "devtools", "roxygen2", "usethis"))
+
+# One-time calls to add Rcpp and Armadillo as dependencies for C++ compilation
+# usethis::use_package("Rcpp", type = "LinkingTo")
+# usethis::use_package("RcppArmadillo", type = "LinkingTo")
+
+# one-time call to check documentation
+# devtools::check(args = c("--as-cran"))
+
+# install and build from GitHub (WIP)
+# remotes::install_github(
+#   "luveenw/occPlus@luveenw_test_plan_3", build_vignettes = FALSE)
+
+# LOCAL ALTERNATIVE: if you downloaded the code, run this instead and comment
+# out the line above this:
+devtools::install(build_vignettes = FALSE)
+
+# Build binary package
+#
+# NOTE: THIS GENERATES A PLATFORM-SPECIFIC PACKAGE. PACKAGE FILES GENERATED ON WINDOWS WON'T RUN ON MACOS, FOR EXAMPLE
+#
+# devtools::build(binary = TRUE)
+
+library(occPlus)
+getNamespaceExports("occPlus") # lists all exported functions and data
